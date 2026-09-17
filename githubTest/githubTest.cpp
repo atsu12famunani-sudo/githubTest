546f7fbb86e355b79ed51bc10a30fe2b1e0d2ced
atsuki tsuchiya <atsu12jun@outlook.jp>
2026-09-17 13:31:29 +0900

    テスト変更

--- githubTest/githubTest.cpp
+++ githubTest/githubTest.cpp
@@ -2,10 +2,12 @@
 //
 
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    cout << "Hello World!\n";
+    cout << "Enbug-near" << endl;
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
